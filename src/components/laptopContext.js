ff9051d131f23a0f8ebcf78f7104275105c58515
Anuj Shrivastava <117368088+anujshrivastava1@users.noreply.github.com>
2024-04-18 14:28:55 +0530

    Update laptopContext.js

--- src/components/laptopContext.js
+++ src/components/laptopContext.js
@@ -8,7 +8,7 @@ export const LaptopProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get('https://my-json-server.typicode.com/rohitpatil03/mockapi/laptops')
+        axios.get('https://my-json-server.typicode.com/anujshrivastava1/MyDemo/laptops')
             .then((res) => {
                 setCars(res.data);
                 setLoading(false);
